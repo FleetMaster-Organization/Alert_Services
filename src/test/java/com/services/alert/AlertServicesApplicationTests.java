25e92bb07f67e62139564f97300d58d739487adf
Frank_G <guilombofrank07@gmail.com>
2026-03-11 11:23:17 -0500

    initial git again

---
+++ src/test/java/com/services/alert/AlertServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
